89e87f3dd4f801bb02656536a07ab34bcd48c153
tanjimN4 <tanjimahemed7@gmail.com>
2024-10-03 01:06:23 +0600

    add jwt and user,admin api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
+const stripe = require("stripe")(process.env.sk);
 
 //middleware
 app.use(cors())
@@ -30,6 +32,43 @@ async function run() {
     const productCollection = client.db("lifestyle").collection("product");
     const userCollection = client.db("lifestyle").collection("users");
     const addChartCollection = client.db("lifestyle").collection("addChart");
+    const PaymentSuccessCollection = client.db("lifestyle").collection("paymentSuccess");
+    const AdminCheckCollection = client.db("lifestyle").collection("additem");
+
+    //jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+    const verifyToken = (req, res, next) => {
+      const token = req.headers.authorization?.split(' ')[1]; // Extract token
+
+      if (!token) {
+        return res.status(403).send('No token provided');
+      }
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send('Invalid token');
+        }
+        req.decoded = decoded; // Attach decoded token to req object
+        next();
+      });
+    };
+
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'fobidden access' })
+
+      }
+      next()
+    }
 
     app.get('/products', async (req, res) => {
       const products = await productCollection.find().toArray()
@@ -38,9 +77,64 @@ async function run() {
     })
     app.post('/users', async (req, res) => {
       const user = req.body
+      const query = { email: user.email }
+      const exist = await userCollection.findOne(query)
+      if (exist) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
+
+    app.get('/users/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      // console.log('Decoded email from token:', req.decoded.email);
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      const quary = { email: email }
+      const user = await userCollection.findOne(quary);
+      // console.log('User fetched from DB:', user); // Log the user data
+
+      const admin = user?.role === 'admin'; // Check if the role is 'admin'
+      // console.log('Is Admin:', admin); // Log whether the user is an admin or not
+
+      res.send({ admin });
+    });
+
+
+    app.get('/users', async (req, res) => {
+      const users = await userCollection.find().toArray()
+      res.send(users)
+    })
+    //delete user
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+    //make admin
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          role: 'admin',
+        },
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+    })
+
+    //get  PaymentSuccessCollection in  frontend
+    app.get('/paymentSuccess', async (req, res) => {
+      const paymentSuccess = await PaymentSuccessCollection.find().toArray()
+      res.send(paymentSuccess)
+    })
+
     app.post('/add/chart', async (req, res) => {
       const chart = req.body
       const result = await addChartCollection.insertOne(chart)
@@ -56,7 +150,71 @@ async function run() {
       const id = req.params.id
       const result = await addChartCollection.deleteOne({ _id: new ObjectId(id) })
       res.send(result)
-      })
+    })
+    //delete that match user email
+    app.delete('/add/chart', async (req, res) => {
+      const quary = req.query.email
+      const result = await addChartCollection.deleteMany(quary)
+      res.send(result)
+    })
+    //update status
+    app.put('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const {order_status}=req.body
+      const filter = { _id: new ObjectId(id)};
+      const updateDoc = {
+          $set: {
+            order_status: order_status,
+          },
+      };
+      const result = await PaymentSuccessCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  });
+  
+
+    //product data save after payment success
+    app.post('/add/chart/payment', async (req, res) => {
+      const chart = req.body
+      if (Array.isArray(chart)) {
+        const result = await PaymentSuccessCollection.insertMany(chart); // Ensure chart is an array
+        res.send(result);
+      } else {
+        const result = await PaymentSuccessCollection.insertOne(chart)
+        res.send(result)
+      }
+
+
+    })
+    app.get('/add/chart/payment', async (req, res) => {
+      const result = await PaymentSuccessCollection.find().toArray()
+      res.send(result)
+
+    })
+
+    //payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+
+    });
+    //add item
+    app.post('/add/item', async (req, res) => {
+      const item = req.body
+      const result = await AdminCheckCollection.insertOne(item)
+      res.send(result)
+    })
+    app.get('/add/item', async (req, res) => {
+      const products = await AdminCheckCollection.find().toArray()
+      res.send(products)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
